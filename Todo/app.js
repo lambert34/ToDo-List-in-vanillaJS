3d1c0a025683ebce46cd9ab99fd547a32f00c443
lambert34 <aleksandrlambert@gmail.com>
2023-03-26 00:01:08 +0700

    добавил верстку чекбокса для появления задачи

--- Todo/app.js
+++ Todo/app.js
@@ -13,11 +13,19 @@ addButton.addEventListener('click', function(){
     };
 
     todoList.push(newTodo);
+    displayMessages();
 });
 
 function displayMessages(){
 
     todoList.forEach(function(item, i){
-
+    let displayMessage = `
+    <li>
+    <input type='checkbox' id='item_${i}'>
+    <label for='item_${i}'>${item.todo}</label>
+    </li>
+    `;
+    todo.innerHTML = displayMessage;
     });
+
 }
