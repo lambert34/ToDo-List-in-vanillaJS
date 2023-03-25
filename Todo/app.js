22a0aadb2347b02e05dd933d78f720d4cd80c8da
lambert34 <aleksandrlambert@gmail.com>
2023-03-25 23:38:42 +0700

    каждое новое дело будет добавляться в объект, а он в массив

--- Todo/app.js
+++ Todo/app.js
@@ -2,3 +2,16 @@ let addMessage = document.querySelector('.message'),
 addButton = document.querySelector('.add');
 
 let todoList = [];
+
+addButton.addEventListener('click', function(){
+
+    let newTodo = {
+        todo: addMessage.value,
+        checked: false,
+        important: false
+    };
+
+    todoList.push(newTodo);
+
+    console.log(newTodo);
+});
