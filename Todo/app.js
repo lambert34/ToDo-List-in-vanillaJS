6a8a538d1aadb5016a4465174b07d7a9c12e5da8
lambert34 <aleksandrlambert@gmail.com>
2023-03-26 02:16:03 +0700

    добавляет ряд динамических событий

--- Todo/app.js
+++ Todo/app.js
@@ -29,10 +29,30 @@ function displayMessages(){
     displayMessage += `
     <li>
     <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
-    <label for='item_${i}'>${item.todo}</label>
+    <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
     </li>
     `;
     todo.innerHTML = displayMessage;
     });
+};
 
-}
+// todo.addEventListener('change', function(Event)){
+//     let valueLabel = todo.querySelector('[for='+ Event.srcElement.getAttribute('id') +']').innerHTML;
+    
+    todoList.forEach(function(item){
+        if (item.todo === valueLabel){
+            item.checked = !item.checked;
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+
+    todo.addEventListener('contextmenu', function(event){
+        event.preventDefault();
+        todoList.forEach(function(item){
+            if (item.todo === event.target.innerHTML){
+                item.important = !item.important;
+                displayMessages();
+                localStorage.setItem('todo', JSON.stringify(todoList));
+            }
+        });
+    });
